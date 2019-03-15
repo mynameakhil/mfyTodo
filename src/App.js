45b085ca5a147ef54081213f428fa3caca1ddfd9
Akhil krishnan <akhil@mfy.im>
2019-03-15 18:23:41 +0530

    todo with edit

--- src/App.js
+++ src/App.js
@@ -1,59 +1,64 @@
 import React, { Component } from 'react';
 import './App.css';
+import { toUnicode } from 'punycode';
 
 class App extends Component {
   state = {
     noteText: '',
-    notes: []
-
-    
+    notes: [],
+    updteBtn:true,
+    id:''
+  } 
+  editTodo(val,index){
+    this.setState({noteText:val,updteBtn:false,id:index});
+    this.state.notes[index]='';
+    this.setState({notes:[...this.state.notes]});
   }
-  
-updateNoteText(input){
-  this.setState({noteText:input},
-    () => { console.log(this.state.noteText);});
+  updateList(e){
+   const ide=this.state.id;
+    this.state.notes[ide] = this.state.noteText;
+    this.setState({ notes: [...this.state.notes], noteText: "" ,updteBtn:true})
   }
-takeValue(input){
-  let arrNote=this.state.notes;
-  arrNote.push(input);
-  this.setState({noteText:arrNote})
-  this.setState({noteText:''})
-  console.log(input);
-}
-deleteHandler(indx){
-  let arr=[...this.state.notes];
-  arr.splice(indx,1);
-  this.setState({notes:arr})
-
-}
-keyPress(event,input){
-  if(event.key==='Enter'){
-    let arrNote = this.state.notes;
-    arrNote.push(input);
-    this.setState({ noteText: arrNote })
-    this.setState({ noteText: '' })
-    console.log(input);
+  addTodo = (e) =>{
+    if (e.key === 'Enter') {
+     this.setState({notes: [e.target.value, ...this.state.notes], noteText:""})
+    }
   }
-}
-
   render() {
-    return (
-      <div className="App">
-        <h1>TO DO</h1>
-         < input type = "text" className="textInput"
-           value={this.state.noteText}
-          onChange={(e) => {this.updateNoteText(e.target.value)}}
-          onKeyPress={(e) => { this.keyPress(e,this.state.noteText) }} 
-        placeholder = 'Enter the note'/> 
-        <div className="btn" onClick={() => this.takeValue(this.state.noteText)}>+</div>
-        <ul>
-          {this.state.notes.map((val, indux) => <li className="lst">{val}
-          <button className="btn2" onClick={()=>this.deleteHandler(indux)}>delete</button>
-            <button className="btn2" onClick={() => this.deleteHandler(indux)}>edit</button>
+    let lst;
+    if(this.state.updteBtn)
+    {lst=(
+      <ul>
+        <div>
+          {this.state.notes.map((val, index) => <li className="lst">{val}
+            <button  onClick={() => this.setState({ notes: this.state.notes.filter(note => note !== val) })}>delete</button>
+            <button  onClick={() => this.editTodo(val, index)}>edit</button>
           </li>)}
-        </ul>
+        </div>
+      </ul>);
+    }
+    else{
+     lst=( 
+      <div>
+        <button className="btn2" onClick={()=>this.updateList()}>update</button>
+      </div>);
+    }
+
+    return (
+     <div >
+      <div className="App">
+        <h1 >TO DO</h1>
+         <input type = "text" 
+          value={this.state.noteText}
+          onChange={(e) => this.setState({noteText:e.target.value})}
+          onKeyPress={this.addTodo}
+          placeholder = 'Enter the note'/> 
+        <div className="btn" onClick={(e) => this.setState({notes: [this.state.noteText, ...this.state.notes], noteText:""})}>+</div>
+        {lst}
       </div>
-    );
+    </div>
+  
+  );
   }
 }
 
